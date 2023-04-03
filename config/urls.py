bdb892a2f713ef5e7a484b8686afad7c3919d20a
lukaszzych84 <lukasz251984@gmail.com>
2023-04-04 00:43:31 +0200

    Add models,edit view and many templates

--- config/urls.py
+++ config/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 from django.contrib import admin
 
@@ -16,10 +18,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('account.urls'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
