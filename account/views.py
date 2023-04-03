95216c4a50380c9e44917abf8ecf793dd467190a
lukaszzych84 <lukasz251984@gmail.com>
2023-04-03 20:16:56 +0200

    Add many new templates, add view-register

--- account/views.py
+++ account/views.py
@@ -2,8 +2,7 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
-from .forms import LoginForm
-
+from .forms import LoginForm, UserRegistrationForm
 
 
 def user_login(request):
@@ -26,6 +25,27 @@ def user_login(request):
     else:
         form = LoginForm()
     return render(request, 'account/login.html', {'form': form})
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            # Utwórz nowy obiekt użytkownika, ale jeszcze go nie zapisuj
+            new_user = user_form.save(commit=False)
+            # Ustaw hasło
+            new_user.set_password(
+                user_form.cleaned_data['password'])
+            # Zapisz obiekt User
+            new_user.save()
+            return render(request,
+                          'account/register_done.html',
+                          {'new_user': new_user})
+    else:
+        user_form = UserRegistrationForm()
+    return render(request,
+                  'account/register.html',
+                  {'user_form': user_form})
+
 
 @login_required
 def dashboard(request):
